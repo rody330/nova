09a95ea77a8397550a700f9cba9c0d3ee5cc5966
rody330 <bandilenyaba03@gmail.com>
2026-09-23 16:44:02 +0200

    Add service worker

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE = 'nova-v1';
+const ASSETS = ['/nova/', '/nova/index.html', '/nova/manifest.json'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) {
+      return c.addAll(ASSETS);
+    }).then(function() {
+      return self.skipWaiting();
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  if (e.request.method !== 'GET') return;
+  var url = new URL(e.request.url);
+  if (url.origin !== location.origin) return;
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).then(function(res) {
+        if (res && res.status === 200 && res.type === 'basic') {
+          var copy = res.clone();
+          caches.open(CACHE).then(function(c) {
+            c.put(e.request, copy);
+          });
+        }
+        return res;
+      }).catch(function() {
+        return caches.match('/nova/index.html');
+      });
+    })
+  );
+});
